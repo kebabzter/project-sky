796a8dc5ed8832f8b958c028d550be071201a31b
kebabzter <gabzterthesecond@gmail.com>
2022-12-10 14:52:23 +0200

    Update authController.js
    Removed comments

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -6,25 +6,14 @@ const { parseError } = require('../util/parser');
 
 authController.get('/', async (req, res) => {
     const user = req.user;
+    console.log(user);
     if (user) {
         res.status(200).json(user)
     }
 })
 
 authController.post('/register',
-    // body('email').isEmail().withMessage('Invalid email'),
-    // body('password').isLength({ min: 3 }).withMessage('Password must be at least 3 characters long'),
     async (req, res) => {
-        // try {
-        //     const { errors } = validationResult(req);
-        //     if (errors.length > 0) {
-        //         throw errors;
-        //     }
-        //     const token = await register(req.body.username, req.body.email, req.body.password);
-        //     res.json(token);
-        // } catch (error) {
-        //     res.status(400).json({error:error.message})
-        // }
         try {
             const token = await register(req.body.username, req.body.email, req.body.password);
             res.status(201).json(token);
